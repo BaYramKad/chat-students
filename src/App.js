d21a8ed042f07e641e07a7bee962e6cfd51b5b41
Bayram <bayram.kad@gmail.com>
2022-04-24 17:04:09 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
+import React from 'react'
 import './App.css';
+
+import { Route } from 'react-router-dom';
+
+import TitleComponent from './components/TitleComponent.jsx'
+import MainPage from './components/MainPage/MainPage.jsx';
+import Login from './components/LoginPage/Login.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <TitleComponent />
+      <Route path='/login' component={Login} />
+      <Route path='/main' component={MainPage} />
     </div>
   );
 }
---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TitleComponent() {
+  return (
+    <header>Заголовок</header>
+  )
+}
+
+export default TitleComponent
